0c7adb46cc41784033c20d7855dc080d9a61a34b
Michael Shirk <wmichaelshirk@gmail.com>
2019-06-18 14:33:00 -0500

    Redirect after POST, and show all itmes in template.

--- functional_tests.py
+++ functional_tests.py
@@ -53,8 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
