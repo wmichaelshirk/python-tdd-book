78fbaa221aae94adf8bef4b6870e1c4723605091
Michael Shirk <wmichaelshirk@gmail.com>
2019-07-31 14:23:18 -0500

    Trying a thing.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import cd, env, local, run
 REPO_URL = 'https://github.com/wmichaelshirk/python-tdd-book.git'
 
 def deploy():
-    site_folder = f'/home/{env.user}/{env.sitename}'
+    site_folder = f'/home/{env.user}/{env.sitename.replace("."+env.host, "")}'
     run(f'mkdir -p {site_folder}')
     with cd(site_folder):
         _get_latest_source()
