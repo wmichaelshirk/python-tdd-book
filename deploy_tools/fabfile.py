d9a969d7eb891c26504f6865c9f17d5b98c8f83f
Michael Shirk <wmichaelshirk@gmail.com>
2019-07-31 13:51:13 -0500

    Fixed typo.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -31,11 +31,11 @@ def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')  
     append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')  
-    if 'DJANGO_SECRET_KEY' not in current_contents:  
+    if 'SECRET_KEY' not in current_contents:  
         new_secret = ''.join(random.SystemRandom().choices(  
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        append('.env', f'SECRET_KEY={new_secret}')
 
 def _update_static_files():
     run(f'~/virtualenv/{env.dir}/3.7/bin/python manage.py collectstatic --noinput')
