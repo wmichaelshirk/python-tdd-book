344ab39e1ec713ce2e9d76b18334e36618cd33e2
Michael Shirk <wmichaelshirk@gmail.com>
2019-08-13 13:43:53 -0500

    Added more frontend tests.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,8 @@
-var initialize = function () {
-    $('input[name="text"]').on('keypress', function () {
+window.Superlists = {}
+window.Superlists.initialize = function () {
+    function hideErrors() {
         $('.has-error').hide()
-    })
+    }
+    $('input[name="text"]').on('keypress', hideErrors)
+    $('input[name="text"]').on('click', hideErrors)
 }
